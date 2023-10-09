fab786a9762b338de4be3af94fe016738ac38d4b
ArthurRedmond <19720427+ArthurRedmond@users.noreply.github.com>
2023-10-09 18:00:19 +0000

    Challenge 6

--- 01_restaurant/challenges/01_06_removeinfo_ch.sql
+++ 01_restaurant/challenges/01_06_removeinfo_ch.sql
@@ -3,3 +3,11 @@
 -- A customer named Norby has notified us he won't 
 -- be able to keep his Friday reservation. 
 -- Today is July 24, 2022.
+
+SELECT ReservationID, Date, Customers.FirstName, Customers.LastName From Reservations
+LEFT JOIN Customers on Reservations.CustomerID = Customers.CustomerID
+Where Customers.FirstName like '%norby%' 
+  AND Reservations.Date >= '2022-07-24';
+
+DELETE FROM Reservations
+WHERE ReservationID = 2000; 
