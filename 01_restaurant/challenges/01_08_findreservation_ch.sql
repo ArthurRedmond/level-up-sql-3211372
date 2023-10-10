673c118df3b4f8794c5f4ced2a15f67ca2091ca6
ArthurRedmond <19720427+ArthurRedmond@users.noreply.github.com>
2023-10-10 12:36:29 +0000

    Complete Challenge 8

--- 01_restaurant/challenges/01_08_findreservation_ch.sql
+++ 01_restaurant/challenges/01_08_findreservation_ch.sql
@@ -5,3 +5,11 @@
 -- Stevensen, Stephensen, Stevenson, Stephenson, Stuyvesant
 
 -- There are four people in the party. Today is June 14th.
+
+SELECT Res.ReservationID, Res.CustomerID, Cust.LastName, Res.PartySize, REs.Date
+FROM Reservations as Res
+JOIN Customers as Cust 
+  on Res.CustomerID = Cust.CustomerID
+WHERE Cust.LastName in ('Stevensen', 'Stephensen', 'Stevenson', 'Stephenson', 'Stuyvesant')
+  AND Res.PartySize = 4
+  AND Res.Date > '2022-06-14';
