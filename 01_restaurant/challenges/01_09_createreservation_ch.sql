4e3f533e7dc062cfde4cb151141b8056bbed3879
ArthurRedmond <19720427+ArthurRedmond@users.noreply.github.com>
2023-10-10 12:59:37 +0000

    Complete 9

--- 01_restaurant/challenges/01_09_createreservation_ch.sql
+++ 01_restaurant/challenges/01_09_createreservation_ch.sql
@@ -4,3 +4,21 @@
 -- Use the following information:
 -- Sam McAdams (smac@kinetecoinc.com), for 5 people
 -- on August 12, 2022 at 6PM (18:00)
+SELECT * From Customers
+WHERE Email = 'smac@kinetecoinc.com';
+
+INSERT INTO Customers (FirstName, LastName, Email)
+Values ('Sam', 'McAdams', 'smac@kinetecoinc.com');
+
+SELECT * From Customers
+WHERE Email = 'smac@kinetecoinc.com';
+
+INSERT INTO Reservations (
+  CustomerID, Date, PartySize)
+  Values (102, '2022-08-12 18:00:00', 5);
+
+SELECT Cust.FirstName, Cust.LastName, Cust.Email, Res.Date, Res.PartySize
+FROM Customers as Cust
+JOIN Reservations as Res
+  ON Cust.CustomerID = Res.CustomerID
+Where Cust.Email = 'smac@kinetecoinc.com';
