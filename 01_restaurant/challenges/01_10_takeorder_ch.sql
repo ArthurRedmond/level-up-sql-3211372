4cb5a49bf70bd990b3fca1309eac13eee6ef8ee6
ArthurRedmond <19720427+ArthurRedmond@users.noreply.github.com>
2023-10-10 15:47:02 +0000

    Complete 10

--- 01_restaurant/challenges/01_10_takeorder_ch.sql
+++ 01_restaurant/challenges/01_10_takeorder_ch.sql
@@ -7,3 +7,34 @@
 -- 1 Tropical Blue Smoothie
 -- Delivery date and time: September 20, 2022 @ 2PM (14:00)
 -- There are no taxes or other fees.
+
+SELECT * FROM Customers
+WHERE FirstName = 'Loretta';
+
+INSERT INTO Orders (
+  CustomerID, OrderDate)
+  VALUES ( 70, '2022-09-20 14:00:00');
+
+SELECT * FROM Orders
+WHERE CustomerID = 70
+ORDER BY OrderDate Desc
+LIMIT 1;
+
+SELECT DishID, Name, Price 
+FROM Dishes
+WHERE Name LIKE 'House Salad' OR 
+      Name LIKE '%Cheeseburger%' OR
+      Name LIKE '%Blue%';
+
+INSERT INTO OrdersDishes (OrderID, DishID)
+VALUES (1001, (SELECT DishID FROM Dishes WHERE Name = 'House Salad')),
+       (1001, (SELECT DishID FROM Dishes WHERE Name = 'Mini Cheeseburger')),
+       (1001, (SELECT DishID FROM Dishes WHERE Name = 'Tropical Blue Smoothie'));
+
+SELECT * FROM OrdersDishes
+WHERE OrderID = 1001;
+
+Select SUM(Dishes.Price) AS TotalPrice
+From OrdersDishes
+JOIN Dishes ON OrdersDishes.DishID = Dishes.DishID
+WHERE OrdersDishes.OrderID = 1001
